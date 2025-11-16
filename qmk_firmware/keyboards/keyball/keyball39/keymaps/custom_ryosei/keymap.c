9856104adf421a29ab99addbb6592b6b01fad118
ryosei18 <ryoshu18@gmail.com>
2025-11-16 18:10:27 +0900

    ENTキーとBSPCキーの位置を親指に戻す

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/custom_ryosei/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/custom_ryosei/keymap.c
@@ -23,6 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define BSPC_SHT SFT_T(KC_BSPC)
 #define SPC_LT1 LT(1, KC_SPC)
+#define ENT_LT2 LT(2, KC_ENT)
 #define L_LT3 LT(3, KC_L)
 
 #define SCRN_SHT LSFT(LGUI(KC_S))
@@ -97,9 +98,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
     Q_ESC    , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
-    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , L_LT3    , BSPC_SHT ,
-    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , COMM_ECXL, DOT_QUES , KC_ENT   ,
-    _______  , _______  , _______  , _______  , SPC_LT1  , KC_LSFT  ,      KC_LCTL  , MO(2)  , _______  , _______  , _______  , KC_DEL
+    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , L_LT3    , KC_LALT  ,
+    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , COMM_ECXL, DOT_QUES , KC_DEL   ,
+    _______  , _______  , _______  , _______  , SPC_LT1  , BSPC_SHT ,      KC_LCTL  , ENT_LT2  , _______  , _______  , _______  , _______
   ),
 
   [1] = LAYOUT_universal(
