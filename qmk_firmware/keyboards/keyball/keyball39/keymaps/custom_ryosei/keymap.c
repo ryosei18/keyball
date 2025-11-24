bb82a4d694d2b7622fd47de23f3ffe73000fe6d8
ryosei18 <ryoshu18@gmail.com>
2025-11-24 14:34:00 +0900

    J・FキーをALTMODに変更

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/custom_ryosei/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/custom_ryosei/keymap.c
@@ -23,6 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define BSPC_SHT SFT_T(KC_BSPC)
 #define ALT_SHT SFT_T(KC_LALT)
+#define J_ALT ALT_T(KC_J)
+#define F_ALT ALT_T(KC_F)
 #define SPC_LT1 LT(1, KC_SPC)
 #define ENT_LT2 LT(2, KC_ENT)
 #define L_LT3 LT(3, KC_L)
@@ -99,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
     Q_ESC    , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
-    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , L_LT3    , ALT_SHT  ,
+    KC_A     , KC_S     , KC_D     , F_ALT    , KC_G     ,                            KC_H     , J_ALT    , KC_K     , L_LT3    , ALT_SHT  ,
     KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , COMM_ECXL, DOT_QUES , KC_DEL   ,
     _______  , _______  , _______  , _______  , SPC_LT1  , BSPC_SHT ,      KC_LCTL  , ENT_LT2  , _______  , _______  , _______  , _______
   ),
